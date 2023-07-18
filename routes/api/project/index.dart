6369b210d49232eca2f14c196499ce22fb4f8a6a
Manil Pun <punmanil3@gmail.com>
2023-07-18 12:12:00 +0545

    get project list api

---
+++ routes/api/project/index.dart
@@ -0,0 +1,64 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+import '../../../models/project/project_model.dart';
+import '../../../services/database_service.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  return DatabaseService.startConnection(
+    context,
+    getProjects(context),
+  );
+}
+
+Future<Response> getProjects(RequestContext context) async {
+  //check if the request is a GET request
+  if (context.request.method == HttpMethod.get) {
+    //check if project is present
+    final params = context.request.uri.queryParameters;
+    if (!params.containsKey('id')) {
+      final docs = await DatabaseService.projectsCollection.find().toList();
+      final projectsList = docs.map(ProjectModel.fromJson).toList();
+      return Response.json(
+        body: {
+          'success': true,
+          'data': projectsList,
+          'message': 'Projects fetched successfully',
+        },
+      );
+    }
+    //return project with the id
+    final id = params['id'];
+    try {
+      final doc = await DatabaseService.projectsCollection.findOne(
+        where.eq(
+          'project_id',
+          id,
+        ),
+      );
+      if (doc != null && doc.isNotEmpty) {
+        final project = ProjectModel.fromJson(doc);
+        return Response.json(
+          body: {
+            'success': true,
+            'data': project,
+            'message': 'Project fetched successfully',
+          },
+        );
+      }
+    } catch (e) {
+      return Response.json(
+        statusCode: 404,
+        body: {
+          'success': false,
+          'message': 'Project not found',
+        },
+      );
+    }
+  }
+  return Response.json(
+    statusCode: 404,
+    body: {
+      'message': 'Method not allowed',
+    },
+  );
+}
