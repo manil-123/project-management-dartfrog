67a390e3d41a5090cf145c06d444046b6871ddd5
Manil Pun <punmanil3@gmail.com>
2023-07-19 12:12:00 +0545

    api entry point

--- routes/index.dart
+++ routes/index.dart
@@ -1,9 +1,11 @@
 import 'package:dart_frog/dart_frog.dart';
-
 import '../services/database_service.dart';
 
 Future<Response> onRequest(RequestContext context) {
-  return DatabaseService.startConnection(context, onRequestIndex(context));
+  return DatabaseService.startConnection(
+    context,
+    onRequestIndex(context),
+  );
 }
 
 Future<Response> onRequestIndex(RequestContext context) async {
