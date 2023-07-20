d08e8867a6c54d1202bb9025504c0f5e124cce52
Manil Pun <punmanil3@gmail.com>
2023-07-20 12:12:00 +0545

    get tickets api

--- routes/ticket/index.dart
+++ routes/ticket/index.dart
@@ -4,25 +4,32 @@ import '../../models/project/project_model.dart';
 import '../../services/database_service.dart';
 
 Future<Response> onRequest(RequestContext context) async {
-  return DatabaseService.startConnection(context, getSprints(context));
+  return DatabaseService.startConnection(context, getTickets(context));
 }
 
-Future<Response> getSprints(RequestContext context) async {
+Future<Response> getTickets(RequestContext context) async {
   //check if the request is a GET request
   if (context.request.method == HttpMethod.get) {
     //check if user_id is present
     final params = context.request.uri.queryParameters;
-    if (!params.containsKey('id')) {
+    if (!params.containsKey('project_id')) {
       return Response.json(
         statusCode: 404,
         body: {'message': 'Project id is missing'},
       );
     }
+    if (!params.containsKey('sprint_id')) {
+      return Response.json(
+        statusCode: 404,
+        body: {'message': 'Sprint id is missing'},
+      );
+    }
     //get project with the id
-    final id = params['id'];
+    final projectId = params['project_id'];
+    final sprintId = params['sprint_id'];
     try {
       final doc = await DatabaseService.projectsCollection
-          .findOne(where.eq('project_id', id));
+          .findOne(where.eq('project_id', projectId));
       if (doc == null) {
         return Response.json(
           statusCode: 404,
@@ -31,12 +38,21 @@ Future<Response> getSprints(RequestContext context) async {
       }
       if (doc.isNotEmpty) {
         final project = ProjectModel.fromJson(doc);
-        return Response.json(
-          body: {
-            'data': project.sprints,
-            'message': 'Sprints fetched successfully'
-          },
-        );
+        final sprintIndex =
+            project.sprints.indexWhere((element) => element.id == sprintId);
+        if (sprintIndex == -1) {
+          return Response.json(
+            statusCode: 404,
+            body: {'message': 'Sprint not found in the project'},
+          );
+        } else {
+          return Response.json(
+            body: {
+              'data': project.sprints[sprintIndex].tickets,
+              'message': 'Tickets fetched successfully'
+            },
+          );
+        }
       }
     } catch (e) {
       print(e);
